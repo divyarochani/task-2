9f2d96061ca260e6350da8e983dbc21633f61d33
Divya Rochani <119523148+divyarochani@users.noreply.github.com>
2024-08-29 03:47:13 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuBtn = document.getElementById('menu-btn')
+let sidebar = document.getElementById('sidebar')
+let navlinks = document.querySelectorAll('link')
+
+const toggleSidebar = () =>{
+    sidebar.classList.toggle('show_sidebar')
+}
